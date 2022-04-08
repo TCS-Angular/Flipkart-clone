cb0483b0a8cd8a4ca39ab98b85df1f16fc9b4034
ngactive99 <ngactive99@gmail.com>
2022-04-08 17:50:43 +0530

    created header for our app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
