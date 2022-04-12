5e08b5df286534bbb1e040ab960ecdf728de8000
ngactive99 <ngactive99@gmail.com>
2022-04-12 14:25:05 +0530

    add to cart functionality implemented

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -7,14 +8,20 @@ import { ApiService } from 'src/app/service/api.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  public productList: any ;
-  constructor(private api: ApiService){ }
+  public productList: any;
+  constructor(private api: ApiService, private cartService: CartService){ }
 
   ngOnInit(): void {
     this.api.getProduct()
     .subscribe(res => {
       this.productList = res;
+      this.productList.forEach((a:any) => {
+        Object.assign(a,{quantity:1, total:a.price})
+      })
     })
+  }
+  addtocart(item :any) {
+    this.cartService.addtoCart(item)
   }
 
 }
